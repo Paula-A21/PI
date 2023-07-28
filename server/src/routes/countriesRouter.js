97b3dbf4109008ebc5a6ea0907b08c826a08a680
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2023-07-28 14:34:59 -0300

    asdsa

--- server/src/routes/countriesRouter.js
+++ server/src/routes/countriesRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const { getAllCountries, getCountryById, getCountryByName } = require('../handlers/countriesHandlers')
+const {paginationHandler} = require('../handlers/paginationHandler');
 
 const countriesRouter = Router();
 
-countriesRouter.get('/', getAllCountries)
-countriesRouter.get('/name', getCountryByName)
-countriesRouter.get('/:id', getCountryById)
+countriesRouter.get('/', getAllCountries);
+countriesRouter.get('/', paginationHandler);
+countriesRouter.get('/name', getCountryByName);
+countriesRouter.get('/:id', getCountryById);
 
 module.exports = countriesRouter;
